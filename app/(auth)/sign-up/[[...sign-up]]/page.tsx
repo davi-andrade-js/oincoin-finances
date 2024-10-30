57893e9953d6fbafab6798a2e36200d68496f376
Davi <dv.andrade04@gmail.com>
2024-10-30 17:11:24 -0300

    feat(auth): hono.js setup

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Loader2 } from "lucide-react";
 import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+
 export default function Page() {
   return (
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
@@ -22,6 +23,7 @@ export default function Page() {
           </ClerkLoading>
         </div>
       </div>
+
       <div className="h-full bg-blue-600 hidden lg:flex items-center justify-center">
         <Image src="/logo.svg" height={100} width={100} alt="logo" />
       </div>
